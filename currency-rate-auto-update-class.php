346f196fdf77e5f66899e282dfe649ac488c9faf
Fattain Naime <foryou.fn@gmail.com>
2025-08-19 09:04:30 +0600

    V1.1.0

--- currency-rate-auto-update-class.php
+++ currency-rate-auto-update-class.php
@@ -8,15 +8,15 @@ if (!defined('pp_allowed_access')) {
 */
 
 $plugin_meta = [
-    'Plugin Name' => 'Currency Rate Auto Update',
-    'Description' => 'Automatically updates currency conversion rates daily using ExchangeRate-API. Base currency = PipraPay default currency.',
-    'Version' => '1.0.0',
+    'Plugin Name' => 'FX Rate Sync',
+    'Description' => 'Automatically updates currency rates for PipraPay. Supports ExchangeRate-API (v6) and Free Currency Exchange Rates API (jsDelivr).',
+    'Version' => '1.1.0',
     'Author' => 'Fattain Naime',
     'Author URI' => 'https://iamnaime.info.bd/',
     'License' => 'GPL-2.0+',
     'License URI' => 'https://www.gnu.org/licenses/gpl-2.0.html',
     'Requires at least' => '1.0.0',
-    'Plugin URI' => 'https://github.com/fattain-naime/Currency-Rate-Auto-Update-for-PipraPay',
+    'Plugin URI' => 'https://github.com/fattain-naime/currency-rate-auto-update',
     'Text Domain' => '',
     'Domain Path' => '',
     'Requires PHP' => '7.4'
